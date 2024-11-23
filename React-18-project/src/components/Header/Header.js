bd183665acbd8c9477c19bf0c71e654c285815e2
Kha Tran <tuankhavntv@gmail.com>
2024-11-23 11:57:01 +0700

    no message

---
+++ React-18-project/src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import './Header.scss'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        // redicrect to SSO
+        window.location.href = `${process.env.REACT_APP_BACKEND_SSO}?serviceURL=${process.env.REACT_APP_SERVICE_URL}`;
+    }
+
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink to="/" className="nav-link" > Home </NavLink>
+                            <NavLink to="/about" className="nav-link"> About</NavLink>
+                        </Nav>
+
+                        <Nav>
+                            <NavDropdown title="Setting" id="basic-nav-dropdown">
+                                <NavDropdown.Item onClick={() => handleLogin()}> Login </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+
+export default Header;
